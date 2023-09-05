249c7ad083f5c7daf09e80b72e48475cc03c6cda
adhishanand9 <adhishanand9@gmail.com>
2023-09-05 18:13:39 +0530

    Add files for Socket

--- public/js/script.js
+++ public/js/script.js
@@ -54,6 +54,11 @@ function getAllCats(){
     });
 }
 
+let socket = io();
+socket.on('number',(msg)=>{
+    console.log('Random Number: ' + msg);
+})
+
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('#formSubmit').click(()=>{
--- server.js
+++ server.js
@@ -3,12 +3,27 @@ var app = express()
 require('./dbConnection');
 var port = process.env.port || 3000;
 let router = require('./routers/router');
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/cat', router);
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('something');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10)
+
+        )
+    },1000)
+})
+http.listen(3000, () => {
     console.log('express server started');
 }); 
